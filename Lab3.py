c305d1ce8c32ab2471266ed6f148429596e29787
Jasmeen kaur <jkaur592@myseneca.ca>
2024-02-12 13:19:18 -0500

    adding Hello world code to files

--- Lab3.py
+++ Lab3.py
@@ -1,10 +1,6 @@
-def write_text_file_with_name(your_name):
-file_name = "my_name.txt"
-# Open the file in write mode
-with open(file_name, 'w') as file:
-    # Write your name to the file
-    file.write("My name is: " + your_name)
+def helloWorld():
+	print(‘Hello World’)
 
-# Call the function with your name
-your_name = "Your Name"  
-write_text_file_with_name(your_name)
+
+helloWorld()
+
